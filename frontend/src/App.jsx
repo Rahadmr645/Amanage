7c03cbaa189c757ecc374791fc9b001ce7a7c1c8
Rahad <rahadmr645@gmail.com>
2025-05-18 18:27:46 +0300

    initial commit

---
+++ frontend/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './App.css'
+import Home from './pages/Home'
+const App = () => {
+  return (
+    <div className='ms-3'>
+    <Home/>
+    </div>
+
+  )
+}
+
+export default App
